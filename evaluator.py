4b308c1c7a71b2dc62c32b18f8938d3571776c4d
MikiyasMengistu <mikiyasalehegn17@gmail.com>
2022-12-08 11:48:46 +0200

    Lottery version2 added

--- evaluator.py
+++ evaluator.py
@@ -1,27 +1,62 @@
+import random
+
+from utilities import Backend
 from show import Show
 
 
-class Evaluator(Show):
-    """In this Class the winning scenarios had been created to notice a player the result of the game."""
+class Evaluator(Show, Backend):
+    """In this Class the intersection of user's lucky number(self.userwhiteball) and today's powerball winning
+    number(self.todayswhiteball) will be computed in order to create the wining scenarios"""
+    def __init__(self):
+        super().__init__()
+        self.ticket = random.randint(1000,9999)
+
     def compute(self, intersect):
         if intersect == 5 and self.userpowerball == self.todayspowerball:
             print("Congratulations!\nYou have correct White Balls and the Powerball\n you win $324,000,000")
+            identify = int(input("Enter your id: "))
+            account = int(input("Enter your bank account: "))
+            self.record(identify,account,324000000)
         elif intersect == 5 and self.userpowerball != self.todayspowerball:
             print("Congratulations!\nYou have correct White Balls but not the Powerball\n you win $1,000,000")
+            identify = int(input("Enter your id: "))
+            account = int(input("Enter your bank account: "))
+            self.record(identify, account, 1000000)
         elif intersect == 4 and self.userpowerball == self.todayspowerball:
             print("Congratulations!\nYou have 4 correct White Balls and the Powerball\n you win $10,000")
+            identify = int(input("Enter your id: "))
+            account = int(input("Enter your bank account: "))
+            self.record(identify, account, 10000)
         elif intersect == 4 and self.userpowerball != self.todayspowerball:
             print("Congratulations!\nYou have 4 correct White Balls but not the Powerball\n you win $100")
+            identify = int(input("Enter your id: "))
+            account = int(input("Enter your bank account: "))
+            self.record(identify, account, 100)
         elif intersect == 3 and self.userpowerball == self.todayspowerball:
             print("Congratulations!\nYou have 3 correct White Balls and the Powerball\n you win $100")
+            identify = int(input("Enter your id: "))
+            account = int(input("Enter your bank account: "))
+            self.record(identify, account, 100)
         elif intersect == 3 and self.userpowerball != self.todayspowerball:
             print("Congratulations!\nYou have 3 correct White Balls but not the Powerball\n you win $7")
+            identify = int(input("Enter your id: "))
+            account = int(input("Enter your bank account: "))
+            self.record(identify, account, 7)
         elif intersect == 2 and self.userpowerball == self.todayspowerball:
             print("Congratulations!\nYou have 2 correct White Balls and the Powerball\n you win $7")
+            identify = int(input("Enter your id: "))
+            account = int(input("Enter your bank account: "))
+            self.record(identify, account, 7)
         elif intersect == 1 and self.userpowerball == self.todayspowerball:
             print("Congratulations!\nYou have 1 correct White Balls and the Powerball\n you win $4")
+            identify = int(input("Enter your id: "))
+            account = int(input("Enter your bank account: "))
+            self.record(identify, account, 4)
         elif intersect == 0 and self.userpowerball == self.todayspowerball:
             print("Congratulations!\nYou have no correct White Balls but you have the Powerball\n you win $4")
+            identify = int(input("Enter your id: "))
+            account = int(input("Enter your bank account: "))
+            self.record(identify, account, 4)
         else:
             print("Try again")
 
--- utilities.py
+++ utilities.py
@@ -1,9 +1,12 @@
 from random import randint
+import pyodbc
+import datetime
 
 
 class Creatnumbers:
     """Under this class powerball numbers and users lucky number will be created inside create method.
     After each digit are created it will be appended to each empty lists accordingly"""
+
     def __init__(self):
         self.userwhiteball = []
         self.todayswhiteball = []
@@ -23,5 +26,21 @@ class Creatnumbers:
         self.todayspowerball.append(todaypowerball)
 
 
+class Backend:
+    """This class consists of functions that will keep winners data on the national lottery institute's database"""
 
+    def __init__(self):
+        self.ticket = randint(1000, 9999)
 
+    def record(self, p_id, account_num, money: int):
+        connect = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL '
+                                 'Server};SERVER=MIKE;DATABASE=Lotto;Trusted_Connection=yes;')
+
+        cursor = connect.cursor()
+        cursor.execute("insert into [Winners] ([Ticket_id],[Personal_id],[Date_time],[Bank_account],[Amount]) "
+                       "values(?,?,?,?,?)", (self.ticket, p_id, datetime.datetime.now(), account_num, money))
+
+        cursor.commit()
+
+        cursor.close()
+        connect.close()
